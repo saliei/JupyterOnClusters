4e0d3e8f3214b429877505b980d88c7937f853c1
saliei <saeid.aliei@gmail.com>
2023-05-14 13:17:48 +0200

    find a port for jupyter kernel

--- slurm.sh
+++ slurm.sh
@@ -17,8 +17,8 @@
 
 module purge
 module load anaconda
-# acivate the conda environmnet
-conda activate <conda-env>
+# acivate an specific conda environmnet
+# conda activate <conda-env>
 # for R notebooks load the R module first
 # module load R
 # for Julia notebooks load the Julia module first
@@ -26,16 +26,27 @@ conda activate <conda-env>
 # for exporting notebooks to a PDF, load texlive module
 # module load texlive
 
-# TODO: check if nc is installed
 # if JUPYTER_PORT environment variable is set it will be used,
 # if it's in use we will try to find one in the range 8800-8899,
-# if noting can be found in that range, a random one will be used.
+# if no port can be found in that range, a random one will be used.
 if [[ -z "${JUPYTER_PORT}" ]]; then
     JUPYTER_PORT=$(netstat -antp 2>/dev/null | grep :88 | sort | head -n 1 | awk '{print $4}' | cut -d ":" -f 2)
-    echo $JUPYTER_PORT
+    if [[ -z "${JUPYTER_PORT}" ]]; then
+        RAND_PORT=$(shuf -i 18000-18500 -n 1)
+        # check if the random port is open
+        nc -z localhost $RAND_PORT
+        if [[ $? -eq 0 ]]; then
+            JUPYTER_PORT="${RAND_PORT}"
+        else
+            echo "ERROR: couldn't find a port!"
+            exit 1
+        fi
+    fi
 else
     JUPYTER_PORT="${JUPYTER_PORT}"
 fi
+echo "WARNING: setting notebook port to: $JUPYTER_PORT"
 
-cat /etc/hosts
-jupyter lab --ip=0.0.0.0 --port=8888
+
+#cat /etc/hosts
+#jupyter lab --ip=0.0.0.0 --port=8888
